0d94b49cca8fccf2087ee78ccd0f35dfdd67fe21
Joey Rivera <joeyrivera@air-watch.com>
2014-02-07 20:31:22 -0500

    using config instead of hardcoding values.

--- public/tests/constantcontact.php
+++ public/tests/constantcontact.php
@@ -6,8 +6,8 @@ require_once '../../config/config.php';
 $config = new \Zend\Config\Config(require '../../config/config.php');
 
 use Ctct\ConstantContact;
-$cc = new ConstantContact('vynubzznpjryt58qquctpccf');
+$cc = new ConstantContact($config['ccApiKey']);
 
-$contacts = $cc->getContacts('926089ce-d56a-42ee-a75c-510a17360313');
+$contacts = $cc->getContacts($config['ccApiToken']);
 
 var_dump($contacts);
